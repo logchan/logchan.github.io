a895161c0006a226f44e181401a9113ed6cd85bd
logchan <logchan@outlook.com>
2024-09-25 09:40:04 -0400

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
+import Footer from "./footer";
 import Gear from "./gear";
 import PersonalLinks from "./personal-links";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-12 items-center min-h-svh">
-      <div className="h-40 flex flex-col-reverse sm:h-48">
-        <h1 className="text-8xl font-mono hover:text-9xl transition-all items-end border-b-2 border-b-orange-600 pb-4">logchan</h1>
-      </div>
-      <PersonalLinks />
+    <main className="flex flex-col justify-between items-center min-h-svh">
+      <section className="flex flex-col gap-12 items-center">
+        <div className="h-40 flex flex-col-reverse sm:h-48">
+          <h1 className="text-8xl font-mono hover:text-9xl transition-all items-end border-b-2 border-b-orange-600 pb-4">logchan</h1>
+        </div>
+        <PersonalLinks />
+      </section>
       <section>
         <h2 className="text-gray-200 text-6xl mt-20">Coming Soon</h2>
       </section>
+      <Footer />
       <Gear />
     </main>
   );
